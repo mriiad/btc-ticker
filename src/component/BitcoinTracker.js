a8d75cbff72bc599a5cde37c1017302590ffc8a6
MOHAMED RIAD <mohamedriiad@gmail.com>
2021-11-16 22:39:43 +0100

    Infinite API calls & updating btc value

--- src/component/BitcoinTracker.js
+++ src/component/BitcoinTracker.js
@@ -2,7 +2,23 @@ import { useEffect, useState } from "react";
 
 function BitcoinTracker() {
   const [bitcoinPrice, setBitcoinPrice] = useState(0);
+
+  // First API call to avoid waiting 5s when the page is loaded for the first time
+  useEffect(() => trackBitcoinValue(), []);
+
+  // API calls every 5 seconds
   useEffect(() => {
+    async function fetchBtcValue() {
+      const handleTracking = await setInterval(trackBitcoinValue, 5000);
+
+      return () => {
+        clearInterval(handleTracking);
+      };
+    }
+    fetchBtcValue();
+  }, []);
+
+  const trackBitcoinValue = () => {
     const BITCOIN_URL = "https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR";
 
     fetch(BITCOIN_URL)
@@ -12,11 +28,12 @@ function BitcoinTracker() {
       .then((responseData) => {
         setBitcoinPrice(responseData.result.XXBTZEUR["a"][0] + " €");
       });
-  }, []);
+  };
+
   return (
     <div>
       <h2>price: {bitcoinPrice}</h2>
-      <img src="./1016331-middle.png" alt="bitcoin-logo" />
+      <img src="./btc-icon.gif" alt="bitcoin-logo" />
     </div>
   );
 }
