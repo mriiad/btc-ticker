055cfdf6ab66ab6f7709b3e558d7eb6135421fa7
MOHAMED RIAD <mohamedriiad@gmail.com>
2021-11-16 22:56:59 +0100

    Adding meta tag and changing app title

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import BitcoinTracker from "./component/BitcoinTracker";
 import logo from "./logo.svg";
 
 function App() {
+  document.title = "Bitcoin Ticker App";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Bitcoin ticker using React</h2>
         <BitcoinTracker />
       </header>
     </div>
